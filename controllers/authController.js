05ccfba9fedd1963603bbb64600f02bc5b56c534
NKC <khacchang128@gmail.com>
2022-12-31 20:49:40 +0700

    fix login trycat

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,30 @@
 import User from "../models/User.js";
 
 export const register = async (req, res) => {
-  const usernameExists = await User.findOne({ username: req.body.username });
-  if (usernameExists) {
-    return res.status(400).json({ msg: "Username already exists" });
-  }
+  try {
+    const usernameExists = await User.findOne({ username: req.body.username });
+    if (usernameExists) {
+      return res.status(400).json({ msg: "Username already exists" });
+    }
 
-  const user = await User.create(req.body);
-  res.status(201).json({ msg: "Register successful", user });
+    const user = await User.create(req.body);
+    res.status(201).json({ msg: "Register successful", user });
+  } catch (error) {
+    return res.status(400).json(error);
+  }
 };
 
 export const login = async (req, res) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username, password });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
 
-  if (!user) {
-    res.status(400).json({ msg: "Username or Password incorrect" });
+    if (!user) {
+      res.status(400).json({ msg: "Username or Password incorrect" });
+    }
+
+    res.status(200).json({ msg: "Login successful", user });
+  } catch (error) {
+    return res.status(400).json(error);
   }
-
-  res.status(200).json({ msg: "Login successful", user });
 };
